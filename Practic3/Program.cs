7c13d431da89910629c2600e60ac550d86b08a30
Roman100500 <chizhkinra001@mail.ru>
2022-10-19 17:47:04 +0300

    Practics complete

---
+++ Practic3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+bool Quad(int n1, int n2)
+
+{
+    if (n1 == n2 * n2 || n2 == n1 * n1)
+        return true;
+    else
+        return false;
+}
+
+Console.Write("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool result = Quad(a, b);
+Console.WriteLine(result);
